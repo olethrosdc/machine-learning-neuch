5faf7dcade5f8d6a935d7580594541559572f5d3
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2023-01-18 09:46:24 +0100

    Background and plan

--- src/NearestNeighbours/NearestNeighbourClassifier.py
+++ src/NearestNeighbours/NearestNeighbourClassifier.py
@@ -1,5 +1,9 @@
+def euclidean_metric(x, y):
+    return (x - y)
+
+## Skeleton code to be filled in
 class NearestNeighbourClassifier:
-    def __init__(self, D):
+    def __init__(self, data, metric):
         pass
     def predict(self, x):
         pass
