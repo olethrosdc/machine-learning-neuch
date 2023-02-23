503c0f0490325aacb66f6fbb80b043fa6d665be6
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2023-02-23 13:25:28 +0100

    Background update

--- src/NearestNeighbours/NearestNeighbourClassifier.py
+++ src/NearestNeighbours/NearestNeighbourClassifier.py
@@ -1,13 +1,21 @@
+import numpy as np
+
 def euclidean_metric(x, y):
-    return (x - y)
+    return np.norm(x - y)
 
 ## Skeleton code to be filled in
 class NearestNeighbourClassifier:
+    ## Initialise the neighbours with a specific metric function and dataset
     def __init__(self, data, metric):
+        self.metric = metric
+        self.data = data
+        self.n_
         pass
+    ## predict the most likely label
     def predict(self, x):
         pass
-    def get_probability(self, y):
+    ## return a vector of 
+    def get_probabilities(self, x):
         pass
     
     
