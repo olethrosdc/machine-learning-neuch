d9ffc1aa28b66917092373d8ac9e3e365fa40772
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2023-07-12 06:19:30 +0200

    Minor

--- src/NeuralNetworks/perceptron.py
+++ src/NeuralNetworks/perceptron.py
@@ -17,9 +17,33 @@ plt.show()
 ## Iterate
 def perceptron(features, classes, iterations=100):
     # initialise parameters
-    params = np.zeros(1 + features.shape[1]) # add one more fake feature
-
-    for t in range(iterations):
+    w_t = np.random.uniform(size=1 + features.shape[1]) # add one more fake feature
+    n_data = features.shape[0]
+    labels = np.zeros(n_data)
+    for k in range(iterations):
         # calculate output
-        x_t = np.block([features[t],np.array([1])])
-        y_t = 
+        n_errors = 0
+        for t in range(n_data):
+            ## TO DO: fill in
+            ## count the number of errors
+            ## adjust w_t
+            ## save the predicted label
+    print("error rate: ", n_errors / n_data)
+    return w_t, labels
+
+
+## call the algorithm
+w, labels = perceptron(features, classes, 100, 1)
+
+## plot the results
+
+plt.plot(features[labels==-1,0], features[labels==-1,1], 'r.', alpha=0.5)
+plt.plot(features[labels==1,0], features[labels==1,1], 'g.', alpha=0.5)
+X = np.linspace(-10,10)
+# in our parametrisation, we have
+# a = w[0] + x w[1] + y w[2] 
+# so as a = 0 is the decision boundary, we can solve for the y coordinate
+Y = - (w[0] + X * w[1])/w[2]
+plt.plot(X, Y)
+plt.axis([-10,10,-10,10])
+plt.grid()
