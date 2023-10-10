69503d5322d756f3b2a02060d1bbcd2e74e0f64d
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2023-10-10 10:39:47 +0200

    Fixed labels

--- src/NeuralNetworks/perceptron.py
+++ src/NeuralNetworks/perceptron.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 ## Generate some random data of two clases
 mean = [0, 0]
-covariance = [[10,0], [0,10]]
+covariance = [[10,1], [1,10]]
 n_samples = 1000
 features = np.random.multivariate_normal(mean, covariance, n_samples)
 classes = np.random.choice(2, size=n_samples)
@@ -25,10 +25,17 @@ def perceptron(features, classes, iterations=100):
         n_errors = 0
         for t in range(n_data):
             ## TO DO: fill in
-            ## count the number of errors
-            ## adjust w_t
-            ## save the predicted label
-    print("error rate: ", n_errors / n_data)
+            x_t = np.concatenate([np.array([1]), features[t]])
+            #print(x_t)
+            labels[t] = np.sign(np.dot(w_t, x_t))
+            if (classes[t] == 0):
+                c = -1
+            else:
+                c = +1
+            if (c != labels[t]):
+                w_t += c * x_t
+                n_errors += 1
+        print("error rate: ", n_errors / n_data)
     return w_t, labels
 
 
@@ -47,3 +54,4 @@ Y = - (w[0] + X * w[1])/w[2]
 plt.plot(X, Y)
 plt.axis([-10,10,-10,10])
 plt.grid()
+plt.show()
