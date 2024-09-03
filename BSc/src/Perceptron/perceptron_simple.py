f23bb7baaebd070e0f72393d8ca4ef226230fcfe
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2024-09-03 14:59:01 +0200

    interactive

--- BSc/src/Perceptron/perceptron_simple.py
+++ BSc/src/Perceptron/perceptron_simple.py
@@ -12,5 +12,15 @@ data = [[180, 1],
         [178, 0],
         [165, 1]]
 
+X = np.array(data)
 
-        
+T = X.shape[0]
+plt.ion()
+threshold = 150
+for t in range(T):
+    plt.clf()
+    plt.axis([150,220,-1, 2])
+    plt.plot(X[:t,0], X[:t,1], 'o')
+    plt.plot([threshold, threshold], [-1, 2], '--')
+    plt.pause(0.1)
+    threshold = int(input("place threshold: "))
