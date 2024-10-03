4fdebe589fb8eee80d7b021abda02a02977d8814
Christos Dimitrakakis <christos.dimitrakakis@gmail.com>
2024-10-03 19:18:31 +0200

    Minor

--- BSc/src/Perceptron/perceptron.py
+++ BSc/src/Perceptron/perceptron.py
@@ -3,27 +3,29 @@ import matplotlib.pyplot as plt
 
 ## Generate some random data of two clases
 mean = [0, 0]
-covariance = [[10,1], [1,10]]
-n_samples = 100
+covariance = [[1,0], [0,1]]
+n_samples = 20
 features = np.random.multivariate_normal(mean, covariance, n_samples)
-classes = np.random.choice(2, size=n_samples)
+classes = np.random.choice([-1, 1], size=n_samples) 
 features[classes==1]+=5
 
 #plt.axis('equal')
 #plt.grid()
 #plt.show()
-def display_classifier(features, classes, w, labels):
+def display_classifier(features, classes, w_old, w, labels):
     X = np.linspace(-10,10)
     # in our parametrisation, we have
     # a = w[0] + x w[1] + y w[2] 
     # so as a = 0 is the decision boundary, we can solve for the y coordinate
     Y = - (w[0] + X * w[1])/w[2]
+    Y_old = - (w_old[0] + X * w_old[1])/w_old[2]
     plt.clf()
-    plt.plot(features[classes==0,0], features[classes==0,1], '.', alpha=0.5)
-    plt.plot(features[classes==1,0], features[classes==1,1], '.', alpha=0.5)
-    plt.plot(features[labels==-1,0], features[labels==-1,1], 'r.', alpha=0.5)
-    plt.plot(features[labels==1,0], features[labels==1,1], 'g.', alpha=0.5)
+    plt.plot(features[classes==-1,0], features[classes==-1,1], 'r^', alpha=0.5, s=1)
+    plt.plot(features[classes==1,0], features[classes==1,1], 'g^', alpha=0.5, s=2)
+    plt.plot(features[labels==-1,0], features[labels==-1,1], 'rx', alpha=0.5, s=4)
+    plt.plot(features[labels==1,0], features[labels==1,1], 'gx', alpha=0.5, s=8)
     plt.plot(X, Y)
+    plt.plot(X, Y_old)
     plt.axis([-10,10,-10,10])
     plt.grid()
     plt.pause(0.1)
@@ -36,6 +38,7 @@ def display_classifier(features, classes, w, labels):
 def perceptron(features, classes, iterations=1):
     # initialise parameters randomly
     w_t = np.random.uniform(size=1 + features.shape[1]) # add one more fake feature
+    w_t_old = w_t.copy()
     n_data = features.shape[0]
     labels = np.zeros(n_data)
     for k in range(iterations):
@@ -49,22 +52,27 @@ def perceptron(features, classes, iterations=1):
             labels[t] = np.sign(np.dot(w_t, x_t))
             ## - If the label is wrong...
             if (labels[t] != classes[t]):
+                if (classes[t] > 0):
+                    c = +1
+                else:
+                    c = -1
                 ##   - Move the hyperplane w_t
                 n_errors += 1
-                w_t += labels[t] * x_t
+                w_t += c * x_t
             ## - Else do nothing
             ## Then save the label in labels
             #print(x_t)
-        display_classifier(features, classes, w_t, labels)
+            display_classifier(features[:t+1], classes[:t+1], w_t_old, w_t, labels[:t+1])
+            w_t_old = w_t.copy()
         print("error rate: ", n_errors / n_data)
     return w_t, labels
 
 
 plt.ion()
 ## call the algorithm
-w, labels = perceptron(features, classes, 100)
+w, labels = perceptron(features, classes, 2)
 
 ## plot the results
-display_classifier(features, classes, w, labels)
+display_classifier(features, classes, w, w, labels)
 
 
